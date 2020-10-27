d52a39ed51e44f0387cb5781c9edc24c7b23077b
KamilKwapisz <kamiltm1998@gmail.com>
2020-10-27 08:51:14 +0100

    specified running apps

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
-from appstore import example1, example2
+
+from appstore import app_store, rating_manager
+
 
 app = FastAPI()
 
+
 if __name__ == "__main__":
-    app.include_router(example1.router)
-    app.include_router(example2.router)
-    uvicorn.run(app, host="0.0.0.0", port=8005, reload=True)
-    pass
+    app.include_router(app_store.router)
+    app.include_router(rating_manager.router)
+    uvicorn.run(app, host="0.0.0.0", port=8005)
