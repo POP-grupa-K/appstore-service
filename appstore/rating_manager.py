f2406347b30a74142b203925aae5a3bf90c01de7
KamilKwapisz <kamiltm1998@gmail.com>
2020-10-27 09:24:54 +0100

    rating manager endpoints

--- appstore/rating_manager.py
+++ appstore/rating_manager.py
@@ -1,9 +1,45 @@
-from fastapi import APIRouter
+from typing import Optional
+
+from fastapi import APIRouter, Query
+from pydantic import BaseModel
 
 
 router = APIRouter()
 
 
-@router.get("/example2")
-async def example2():
-    return "example2"
+class RatingItem(BaseModel):
+    user: Optional[str] = None
+    rating: Optional[int] = None
+
+
+@router.get("/{app_uid}", tags=["AppStore Ratings"])
+async def get_rating(
+    app_uid: str,
+    user: str
+):
+    return "Get Rating"
+
+
+@router.post("/{app_uid}", tags=["AppStore Ratings"])
+async def rate_app(
+    app_uid: str,
+    user: str,
+    rating: int = Query(5, title="App rating", gt=0, lte=5)
+):
+    return "Rate App"
+
+
+@router.put("/{app_uid}", response_model=RatingItem, tags=["AppStore Ratings"])
+async def update_rating(app_uid: str, rating: RatingItem):
+    # ...
+    updated_rating = rating
+    # ...
+    return updated_rating
+
+
+@router.delete("/{app_uid}", tags=["AppStore Ratings"])
+async def delete_rating(
+    app_uid: str,
+    user: str
+):
+    return "Delete Rating"
