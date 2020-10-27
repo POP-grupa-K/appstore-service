f95f2a5b0fe14d50f8eb17ff0204d98076ddc680
KamilKwapisz <kamiltm1998@gmail.com>
2020-10-27 09:25:09 +0100

    added endpoint prefixes

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 from appstore import app_store, rating_manager
 
 
-app = FastAPI()
+app = FastAPI(title="System obliczeń wysokoskalowych")
 
 
 if __name__ == "__main__":
-    app.include_router(app_store.router)
-    app.include_router(rating_manager.router)
+    app.include_router(app_store.router, prefix="/appstore")
+    app.include_router(rating_manager.router, prefix="/ratings")
     uvicorn.run(app, host="0.0.0.0", port=8005)
