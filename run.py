6a15800ca9ba1a0c696e2e9a6782f9ac8fc0237c
KamilKwapisz <kamiltm1998@gmail.com>
2020-10-27 07:43:37 +0100

    added reload param to uvicorn`s running app settings

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ app = FastAPI()
 if __name__ == "__main__":
     app.include_router(example1.router)
     app.include_router(example2.router)
-    uvicorn.run(app, host="0.0.0.0", port=8005)
+    uvicorn.run(app, host="0.0.0.0", port=8005, reload=True)
     pass
