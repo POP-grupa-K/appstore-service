462f4297a6a3187d9e12ead0c098aa7336682288
KamilKwapisz <kamiltm1998@gmail.com>
2020-10-27 09:24:34 +0100

    appstore backend endpoints

--- appstore/app_store.py
+++ appstore/app_store.py
@@ -17,22 +17,22 @@ class AppItem(BaseModel):
     ratings: List[str] = []
 
 
-@router.get("/appstore")
+@router.get("/", tags=["Backend AppStore"])
 async def app_details():
     return "Show AppStore"
 
 
-@router.post("/app/add")
+@router.post("/add", tags=["Backend AppStore"])
 async def add_app():
     return "Add app to AppShelf"
 
 
-@router.delete("/app/{app_uid}/remove")
+@router.delete("/{app_uid}/remove", tags=["Backend AppStore"])
 async def remove_app(app_uid: str):
     return "Remove app from AppShelf"
 
 
-@router.put("/app/{app_uid}", response_model=AppItem)
+@router.put("/{app_uid}", response_model=AppItem, tags=["Backend AppStore"])
 async def update_app(app_uid: str, app: AppItem):
     # ...
     updated_app = app
@@ -40,11 +40,11 @@ async def update_app(app_uid: str, app: AppItem):
     return updated_app
 
 
-@router.post("/app/{app_uid}/rate")
+@router.post("/{app_uid}/rate", tags=["Backend AppStore"])
 async def rate_app(app_uid: str):
     return "Rate app"
 
 
-@router.get("/app/{app_uid}")
+@router.get("/{app_uid}", tags=["Backend AppStore"])
 async def app_details(app_uid: str):
     return "App details"
