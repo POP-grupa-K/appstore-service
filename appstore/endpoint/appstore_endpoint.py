1d5da104660caa291c4bde85313eb1b466e459a0
KatarzynaWolska <kasia.wolska137@gmail.com>
2020-11-16 14:33:59 +0100

    Returning json messages

--- appstore/endpoint/appstore_endpoint.py
+++ appstore/endpoint/appstore_endpoint.py
@@ -14,6 +14,7 @@ from appstore.schema.appstore_schema import AppStoreSchema as AppStoreSchema
 from appstore.schema.rating_schema import RatingSchema
 from appstore.service.appstore_service import create_app, delete_app, update_app, add_app_rate, \
     get_all_apps_as_json_list, get_app_json, save_image, get_image, delete_image, update_image
+from appstore.utils.message_encoder.json_message_encoder import encode_to_json_message
 from appstore.utils.validator.file_validator import validate_image
 from run import SessionLocal
 from fastapi.responses import JSONResponse
@@ -42,10 +43,10 @@ async def list_apps(db: Session = Depends(get_db)):
 async def add_app(app: AppStoreSchema, db: Session = Depends(get_db)) -> str:
     try:
         app_id = create_app(app, db)
-        return JSONResponse(status_code=status.HTTP_201_CREATED, content=app_id)
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=encode_to_json_message(app_id))
 
     except Exception as e:
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=e)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=encode_to_json_message(e))
 
 
 @router.delete("/{id_app}", tags=["Backend AppStore"])
@@ -53,12 +54,12 @@ async def remove_app(id_app: int, db: Session = Depends(get_db)):
     try:
         is_deleted = delete_app(id_app, db)
         if is_deleted is not None and is_deleted:
-            return JSONResponse(status_code=status.HTTP_200_OK, content=id_app)
+            return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message(id_app))
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=f"App with id = {id_app} was not found.")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=encode_to_json_message(f"App with id = {id_app} was not found."))
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=e)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=encode_to_json_message(e))
 
 
 @router.put("/{id_app}", tags=["Backend AppStore"])
@@ -66,18 +67,18 @@ async def put_app(id_app: int, app: AppStoreSchema, db: Session = Depends(get_db
     try:
         is_updated = update_app(id_app, app, db)
         if is_updated is not None and is_updated:
-            return JSONResponse(status_code=status.HTTP_200_OK, content=id_app)
+            return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message(id_app))
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=f"App with id = {id_app} was not found.")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=encode_to_json_message(f"App with id = {id_app} was not found."))
     except Exception as e:
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=e)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=encode_to_json_message(e))
 
 
 @router.post("/{id_app}/rate", tags=["Backend AppStore"])
 async def rate_app(id_app: int, rate: RatingSchema, db: Session = Depends(get_db)):
     res = add_app_rate(id_app, rate, db)
     if res:
-        return JSONResponse(status_code=status.HTTP_200_OK, content=res)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message(res))
     return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
@@ -88,10 +89,10 @@ async def get_app(id_app: int, db: Session = Depends(get_db)):
         if app is not None:
             return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(app))
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=f"App with id = {id_app} was not found.")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=encode_to_json_message(f"App with id = {id_app} was not found."))
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=e)
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=encode_to_json_message(e))
 
 
 @router.post("/img/{id_app}", tags=["Backend AppStore"])
@@ -99,15 +100,15 @@ async def upload_app_img(id_app: int, db: Session = Depends(get_db), image: Uplo
     try:
         validate_image(image)
         save_image(id_app, image, db)
-        return JSONResponse(status_code=status.HTTP_200_OK, content="OK")
+        return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message("OK"))
 
     except InvalidFileNameException:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content="Bad filename. Expected JPG or PNG.")
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=encode_to_json_message("Bad filename. Expected JPG or PNG."))
     except NoSuchAppException:
         return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE,
-                            content=f"App with id = {id_app} was not found.")
+                            content=encode_to_json_message(f"App with id = {id_app} was not found."))
     except ImageAlreadyExistsException:
-        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content="Image already exists.")
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content=encode_to_json_message("Image already exists."))
     except UnsupportedMediaTypeException:
         return JSONResponse(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
     except Exception:
@@ -120,15 +121,15 @@ async def update_app_img(id_app: int, db: Session = Depends(get_db), image: Uplo
     try:
         validate_image(image)
         update_image(id_app, image, db)
-        return JSONResponse(status_code=status.HTTP_200_OK, content="OK")
+        return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message("OK"))
 
     except InvalidFileNameException:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content="Bad filename. Expected JPG or PNG.")
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=encode_to_json_message("Bad filename. Expected JPG or PNG."))
     except NoSuchAppException:
         return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE,
-                            content=f"App with id = {id_app} was not found.")
+                            content=encode_to_json_message(f"App with id = {id_app} was not found."))
     except ImageAlreadyExistsException:
-        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content="Image already exists.")
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content=encode_to_json_message("Image already exists."))
     except UnsupportedMediaTypeException:
         return JSONResponse(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
     except Exception as e:
@@ -150,7 +151,7 @@ async def get_app_img(id_app: int, db: Session = Depends(get_db)):
             else:
                 raise Exception("illegal state of img model")
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content="No such image")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=encode_to_json_message("No such image"))
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -160,10 +161,10 @@ async def get_app_img(id_app: int, db: Session = Depends(get_db)):
 async def delete_app_img(id_app: int, db: Session = Depends(get_db)):
     try:
         delete_image(id_app, db)
-        return JSONResponse(status_code=status.HTTP_200_OK, content="OK")
+        return JSONResponse(status_code=status.HTTP_200_OK, content=encode_to_json_message("OK"))
 
     except NoSuchImageException:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content="No such image.")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=encode_to_json_message("No such image."))
     except Exception as e:
         traceback.print_exc(file=sys.stdout)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
