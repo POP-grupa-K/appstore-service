c8afdec2fdaf3af35c4c2cfb17a233c0e4192f6e
jeleniep <piom20@gmail.com>
2020-11-17 07:57:06 +0100

    Average rating calculation

--- appstore/schema/rating_schema.py
+++ appstore/schema/rating_schema.py
@@ -5,10 +5,18 @@ from pydantic.main import BaseModel
 
 
 class RatingSchema(BaseModel):
-    value: float
-    id_app: int
+    value: float = None
+    id_rating: int = None
+    id_app: Optional[int] = None
     comm: Optional[str] = None
     date_update: Optional[datetime] = None
 
     class Config:
         orm_mode = True
+
+    def json(self):
+        json_dict = {}
+        for k, v in self.__dict__.items():
+            capitalized = ''.join(word.title() for word in k.split('_'))
+            json_dict[capitalized[0].lower() + capitalized[1:]] = v
+        return json_dict
